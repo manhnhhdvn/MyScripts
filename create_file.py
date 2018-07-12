376a88e10f961d33a078ccb1aa4ac3168ad09e35
Nguyen Huu Manh <n-manh@eole.co.jp>
2018-07-12 11:40:31 +0900

    Complete code

--- create_file.py
+++ create_file.py
@@ -1,37 +1,133 @@
 import xlwt
 import json
+import datetime
+from pytz import timezone
+from datetime import date
 
-from datetime import datetime
+data_in_file_total_crashes_count = [] # https://api-dash.fabric.io/graphql?relayDebugName=AppTimeseries
+data_in_file_total_sessions_and_users_count = [] # https://api-dash.fabric.io/graphql?relayDebugName=SessionAndUserMetrics
 
-data_of_total_events_count = [] # crashes count
-data_of_total_sessions_for_builds = [] # all session and user count
+with open('total_crashes_count.json') as f:
+    data_in_file_total_crashes_count = json.load(f)
 
-with open('total_events_count.json') as f:
-    data_of_total_events_count = json.load(f)
+with open('total_sessions_and_users_count.json') as f:
+    data_in_file_total_sessions_and_users_count = json.load(f)
 
-with open('total_sessions_for_builds.json') as f:
-    data_of_total_sessions_for_builds = json.load(f)
+# Work with data_in_file_total_sessions_and_users_count
+data_answers = data_in_file_total_sessions_and_users_count["data"]["project"]["answers"]
+total_sessions_for_builds_key = ""
+dau_by_build_key = ""
+for key in data_answers.keys():
+    if "totalSessionsForBuild" in key:
+        total_sessions_for_builds_key = key
+    elif "dauByBuild" in key:
+        dau_by_build_key = key
+synthesized_build_version = data_answers[total_sessions_for_builds_key][0]["synthesizedBuildVersion"]
 
-# Work with data_of_total_sessions_for_builds
-data_answers = data_of_total_sessions_for_builds["data"]["project"]["answers"]
-list_keys = data_answers.keys()
+## Total session
+time_list_of_total_sessions_for_builds = []
+value_of_total_sessions_for_builds = []
+for item in data_answers[total_sessions_for_builds_key][0]["values"]:
+    time = datetime.datetime.fromtimestamp(int(item["timestamp"])).replace(tzinfo=timezone("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
+    time_list_of_total_sessions_for_builds.append(time)
+    value_of_total_sessions_for_builds.append(int(item["value"]))
 
-# style0 = xlwt.easyxf('font: name Times New Roman, color-index red, bold on',
-#     num_format_str='#,##0.00')
-# style1 = xlwt.easyxf(num_format_str='D-MMM-YY')
+## Total DAU
+time_list_of_dau_by_builds = []
+value_of_dau_by_builds = []
+for item in data_answers[dau_by_build_key]["values"]:
+    time = datetime.datetime.fromtimestamp(int(item["timestamp"])).replace(tzinfo=timezone("Asia/Tokyo")).strftime('%Y-%m-%d %H:%M:%S')
+    time_list_of_dau_by_builds.append(time)
+    value_of_dau_by_builds.append(int(item["value"]))
 
-# wb = xlwt.Workbook()
-# ws = wb.add_sheet('A Test Sheet')
+# Work with data_in_file_total_crashes_count
+data_crashlytics = data_in_file_total_crashes_count["data"]["project"]["crashlytics"]
+app_time_series_key = ""
+for key in data_crashlytics.keys():
+    if "appTimeseries" in key:
+        app_time_series_key = key
 
-# ws.write(0, 0, 1234.56, style0)
-# ws.write(1, 0, datetime.now(), style1)
-# ws.write(2, 0, 1)
-# ws.write(2, 1, 1)
-# ws.write(2, 2, xlwt.Formula("A3+B3"))
+## Total crashed sessison
+time_list_of_total_crashed_sessions = []
+value_of_total_crashed_sessions = []
+for item in data_crashlytics[app_time_series_key]["eventsCount"]:
+    time = datetime.datetime.fromtimestamp(int(item[0])).replace(tzinfo=timezone("Asia/Tokyo")).strftime('%Y-%m-%d %H:%M:%S')
+    time_list_of_total_crashed_sessions.append(time)
+    value_of_total_crashed_sessions.append(int(item[1]))
 
-wb.save('example.xls')
+## Total crashed device
+time_list_of_total_crashed_devices = []
+value_of_total_crashed_devices = []
+for item in data_crashlytics[app_time_series_key]["impactedDevices"]:
+    time = datetime.datetime.fromtimestamp(int(item[0])).replace(tzinfo=timezone("Asia/Tokyo")).strftime('%Y-%m-%d %H:%M:%S')
+    time_list_of_total_crashed_devices.append(time)
+    value_of_total_crashed_devices.append(int(item[1]))
 
-# https://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file
-# https://stackoverflow.com/questions/13437727/python-write-to-excel-spreadsheet
-# https://github.com/python-excel/xlwt
-# https://stackoverflow.com/questions/15789059/python-json-only-get-keys-in-first-level
+if len(time_list_of_total_sessions_for_builds) != len(time_list_of_total_crashed_sessions):
+    print("Data Error!")
+    SystemExit
+
+if len(time_list_of_dau_by_builds) != len(time_list_of_total_crashed_devices):
+    print("Data Error!")
+    SystemExit
+
+# Create file
+work_book = xlwt.Workbook()
+work_sheet = work_book.add_sheet("Statistics")
+style_of_title = xlwt.easyxf('font: name Times New Roman, color-index red, bold on', num_format_str='#,##0.00')
+## Version infor
+work_sheet.write(0, 0, "Version:", style_of_title)
+work_sheet.write(0, 1, synthesized_build_version, style_of_title)
+## Title
+column_of_datetime = 0
+column_of_total_sessions = 1
+column_of_crashed_sessions = 2
+column_of_crash_free_sessions_percent = 3
+column_of_total_users = 4
+column_of_crashed_devices = 5
+column_of_crashed_devices_percent = 6
+work_sheet.write(1, column_of_datetime, "Datetime (Year-Month-Day)", style_of_title)
+work_sheet.write(1, column_of_total_sessions, "All sessions", style_of_title)
+work_sheet.write(1, column_of_crashed_sessions, "Crashes sessions", style_of_title)
+work_sheet.write(1, column_of_crash_free_sessions_percent, "Crash-free sessions (%)", style_of_title)
+work_sheet.write(1, column_of_total_users, "All users", style_of_title)
+work_sheet.write(1, column_of_crashed_devices, "Crashes users", style_of_title)
+work_sheet.write(1, column_of_crashed_devices_percent, "Crash-free users (%)", style_of_title)
+## Content
+### Datetime
+for index, time in enumerate(time_list_of_total_sessions_for_builds):
+    work_sheet.write(index + 2, column_of_datetime, time)
+### All sessions
+for index, value in enumerate(value_of_total_sessions_for_builds):
+    work_sheet.write(index + 2, column_of_total_sessions, value)
+### Crashes sessions
+for index, value in enumerate(value_of_total_crashed_sessions):
+    work_sheet.write(index + 2, column_of_crashed_sessions, value)
+### Crash-free sessions
+for index, value in enumerate(value_of_total_sessions_for_builds):
+    number_of_crashed = value_of_total_crashed_sessions[index]
+    result = ((value - number_of_crashed)*1.0/value) * 100.0
+    work_sheet.write(index + 2, column_of_crash_free_sessions_percent, "{0:.2f}%".format(result))
+
+### All users
+for index, value in enumerate(value_of_dau_by_builds):
+    work_sheet.write(index + 2, column_of_total_users, value)
+### Crashes users
+for index, value in enumerate(value_of_total_crashed_devices):
+    work_sheet.write(index + 2, column_of_crashed_devices, value)
+### Crash-free users
+for index, value in enumerate(value_of_dau_by_builds):
+    number_of_crashed = value_of_total_crashed_devices[index]
+    result = ((value - number_of_crashed)*1.0/value)  * 100.0
+    work_sheet.write(index + 2, column_of_crashed_devices_percent, "{0:.2f}%".format(result))
+
+time_list_of_total_sessions_for_builds
+file_name = "{0}~{1}.xls".format(time_list_of_total_sessions_for_builds[0], time_list_of_total_sessions_for_builds[-1])
+work_book.save(file_name)
+print("------------DONE------------")
+
+# Other infor:
+# Using python version: 2.7.14
+# pytz install: python -m pip install pytz
+#               easy_install --upgrade pytz
+# xlwt: https://github.com/python-excel/xlwt
